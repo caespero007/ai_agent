b4aba8ee8698f9d81859e9251173b514a16d5c08
sarvesh <caespero007@gmail.com>
2026-09-07 19:11:48 +0530

    Build basic AI agent with YouTube tool

--- agent.py
+++ agent.py
@@ -1,2 +1,35 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+from tools import open_youtube
+
 load_dotenv()
-client = openAI()
+
+client = OpenAI()
+
+tools = [
+    {
+        "type": "function",
+        "name": "open_youtube",
+        "description": "Open YouTube in the web browser.",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+        }
+    }
+]
+
+while True:
+
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    response = client.responses.create(
+        model="gpt-5-mini",
+        input=user_input,
+        tools=tools
+    )
+
+    print("AI:", response.output_text)
